338a618dc836ecb03dde92e01767fd62587d9fbb
S3886040 <80365733+S3886040@users.noreply.github.com>
2021-12-21 15:08:13 +1000

    Built structure for A2 and developed nav

--- a2/index.php
+++ a2/index.php
@@ -9,31 +9,46 @@
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
     <link id='stylecss' type="text/css" rel="stylesheet" href="main-style.css?t=<?= filemtime("main-style.css"); ?>">
     <script src='../wireframe.js'></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Cormorant+Garamond&family=Permanent+Marker&display=swap" rel="stylesheet">
   </head>
 
   <body>
 
-    <header class='header'>
+    <header id='home'>
         <div class ='header-content-wrapper'>
             <h1>Lunardo</h1>
-            <div>place holder for logo here</div>
+            <!-- Sourced from https://vector.me/browse/690940/camera_icon -->
+            <img src='../../media/camera_icon.svg' class='logo'>
         </div>  
     </header>
 
     <nav class='nav-bar'>
       <ul class='nav-list'>
-        <li class='nav-item'><a href="/">Home</a></li>
-        <li class='nav-item'><a href="/">About Us</a></li>
-        <li class='nav-item'><a href="/">Seats/Prices</a></li>
-        <li class='nav-item'><a href="/">Now Showing</a></li>
+        <li class='nav-item'><a href="#home">Home</a></li>
+        <li class='nav-item'><a href="#about-us">About Us</a></li>
+        <li class='nav-item'><a href="#seats-prices">Seats/Prices</a></li>
+        <li class='nav-item'><a href="#now-showing">Now Showing</a></li>
       </ul>
     </nav>
 
     <main>
-      <section class='home-content'>
+      <section id='home-content'>
+        <h1 class='splash-header'>Lunardo Cinema</h1>
+        <p>
+            Welcome to our new Website!
+        </p>
       </section>
-      <section class='about-us-section'>
+      <section id='about-us'>
         <h1>About Us</h1>
+      </section>
+      <section id='seats-prices'>
+        <h1>Seats and Prices</h1>
+      </section>
+      <section id='now-showing'>
+        <h1>Now Showing</h1>
       </section>
     </main>
 
